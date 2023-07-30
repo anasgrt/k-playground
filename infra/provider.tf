7a9c5d2fe31e7c22b80db4c663a7cdc59d3354ce
anasnedtech <114829917+anasnedtech@users.noreply.github.com>
2023-07-30 02:52:59 +0200

    feat/add_infra

--- infra/provider.tf
+++ infra/provider.tf
@@ -20,7 +20,7 @@ terraform {
     region         = "eu-west-1"
     bucket         = "kabisa-terraform-statefiles"
     dynamodb_table = "kabisa-terraform-lockfiles"
-    key            = "kabisa-playground-anas/github-actions-oidc.tfstate"
+    key            = "kabisa-playground-anas/github-actions-infra.tfstate"
     encrypt        = true
     role_arn       = "arn:aws:iam::003476575487:role/admin"
     session_name   = "terraform"
